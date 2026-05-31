17caa3f7193da5ba9904331a6f22201b5e083ced
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2026-05-31 20:38:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ logger = logging.getLogger("PinterestDL")
 
 API_ID = int(os.getenv("API_ID", "20898349"))
 API_HASH = os.getenv("API_HASH", "9fdb830d1e435b785f536247f49e7d87")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8702671509:AAFx0FOOUiOpuEgBB1GSho7c3gLA0RNykCI")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8965565378:AAFYFy3tZDL7bETknjVpEyQKy18B9KHp7Eo")
 
 BOT_USERNAME = os.getenv("BOT_USERNAME", "yoripinbot")
 OWNER_ID = int(os.getenv("OWNER_ID", "7728424218"))
